b64555ab3bd6771fc01988fd35eae0fc54b457a1
zyq <flowerdo@126.com>
2019-07-27 20:48:58 +0800

    .

--- dubbo-provider/src/main/java/com/irskj/cloud/dubbo/service/impl/UserServiceImpl.java
+++ dubbo-provider/src/main/java/com/irskj/cloud/dubbo/service/impl/UserServiceImpl.java
@@ -2,11 +2,18 @@ package com.irskj.cloud.dubbo.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.irskj.cloud.dubbo.service.UserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
 public class UserServiceImpl implements UserService {
+
+
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "",value = "10")
+    })
     @Override
     public String getUser() {
         log.info("调用用户服务");
